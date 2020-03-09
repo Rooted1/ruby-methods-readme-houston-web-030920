18a18f062b353af4418da51b8ac8370d8e769789
Ruth Obe <ruthobey@gmail.com>
2020-03-09 16:49:42 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,17 +1,13 @@
 # Use this space to code along with the readme
-def greeting 
-  puts "Hello World"
-end 
 
-greeting
-# phrase = "Hello World!"
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
-# puts phrase
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
